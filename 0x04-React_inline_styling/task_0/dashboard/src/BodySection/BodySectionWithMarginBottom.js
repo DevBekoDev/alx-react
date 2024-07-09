d03a8912bf4b260d5e8eb1eb1fe9472fa3daf60e
DevBekoDev <48620096+DevBekoDev@users.noreply.github.com>
2024-07-09 11:19:38 +0300

    fixed

--- 0x04-React_inline_styling/task_0/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,33 +1,21 @@
+import PropTypes from "prop-types";
 import React, { Component } from "react";
-
-import './BodySection';
 import BodySection from "./BodySection";
-import PropTypes from 'prop-types';
+import "./BodySectionWithMarginBottom.css";
 
 class BodySectionWithMarginBottom extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return(
-            <div className="bodySectionWithMargin">
-                <BodySection {... this.props} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="bodySectionWithMargin">
+        <BodySection {...this.props} />
+      </div>
+    );
+  }
 }
 
-BodySectionWithMarginBottom.defaultProps = {
-    children: <React.Fragment />
-};
-
-BodySectionWithMarginBottom.prpTypes = {
-    title: PropTypes.string.isRequired,
-    children: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.element
-    ])
+BodySectionWithMarginBottom.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
 };
 
 export default BodySectionWithMarginBottom;
--- 0x04-React_inline_styling/task_0/dashboard/src/Notifications/Notifications.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/Notifications/Notifications.js
@@ -1,12 +1,9 @@
-//import React, { Component } from 'react';
-import React, { Component } from 'react';
-import './Notifications.css';
-import { getLatestNotification } from '../utils/utils';
-import closeIcon from '../assets/close-icon.png';
-import NotificationItem from './NotificationItem';
-import PropTypes from 'prop-types';
-import NotificationItemShape from './NotificationItemShape'
-
+import React, { Component } from "react";
+import "./Notifications.css";
+import closeIcon from "../assets/close-icon.png";
+import NotificationItem from "./NotificationItem";
+import PropTypes from "prop-types";
+import NotificationItemShape from "./NotificationItemShape";
 
 class Notifications extends Component {
   constructor(props) {
@@ -16,84 +13,63 @@ class Notifications extends Component {
   }
 
   shouldComponentUpdate(nextProps) {
-    return nextProps.listNotifications.length > this.props.listNotifications.length;
+    return nextProps.length > this.props.listNotifications.length;
   }
 
-  markAsRead = (id) => {
+  markAsRead(id) {
     console.log(`Notification ${id} has been marked as read`);
   }
-  
-  render() {
-    const { displayDrawer, listNotifications } = this.props
 
-    return(
-      <>
-        <div className='menuItem'>
+  render() {
+    return (
+      <React.Fragment>
+        <div className="menuItem">
           <p>Your notifications</p>
         </div>
-        { displayDrawer ? 
+        {this.props.displayDrawer ? (
           <div className="Notifications">
-          <button style={{
-            color: '#3a3a3a',
-            fontWeight: 'bold',
-            background: 'none',
-            border: 'none',
-            fontSize: '15px',
-            position: 'absolute',
-            right: '3px',
-            top: '3px',
-            cursor: 'pointer',
-            outline: 'none',
-          }}
-          aria-label="Close"
-          onClick={(e) => {
-            console.log('Close button has been clicked');
-          }}
-          >
-            <img src={closeIcon} alt="close icon" />
-          </button>
-          {
-            listNotifications.length !== 0 ?
-            <p>Here is the list of notifications</p>
-            : null
-          }
-          
-          <ul>
-            {
-              listNotifications.length === 0 ?
-              <NotificationItem type='default' value='No new notification for now' />
-              : null
-            }
-            {
-              listNotifications.map((val, idx) => {
-                return <NotificationItem
-                type={val.type}
-                value={val.value}
-                html={val.html}
-                markAsRead = {this.markAsRead}
-                key = {val.id}
-                />
-              })
-            }
-            
-          </ul>
-        </div>
-      :
-        null
-      }
-      </>
-    )
+            <button
+              style={{
+                color: "#3a3a3a",
+                fontWeight: "bold",
+                background: "none",
+                border: "none",
+                fontSize: "15px",
+                position: "absolute",
+                right: "3px",
+                top: "3px",
+                cursor: "pointer",
+                outline: "none",
+              }}
+              aria-label="Close"
+              onClick={(e) => {
+                console.log("Close button has been clicked");
+              }}
+            >
+              <img src={closeIcon} alt="close icon" width="10px" />
+            </button>
+            {this.props.listNotifications.length != 0 ? <p>Here is the list of notifications</p> : null}
+            <ul>
+              {this.props.listNotifications.length == 0 ? <NotificationItem type="default" value="No new notification for now" /> : null}
+              {this.props.listNotifications.map((val, idx) => {
+                return <NotificationItem type={val.type} value={val.value} html={val.html} key={val.id} markAsRead={this.markAsRead} id={val.id} />;
+              })}
+            </ul>
+          </div>
+        ) : null}
+      </React.Fragment>
+    );
   }
+}
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
 };
 
 Notifications.defaultProps = {
   displayDrawer: false,
-  listNotifications: []
+  listNotifications: [],
 };
-
-Notifications.propType = {
-  displayDrawer: PropTypes.bool,
-  listNotifications: PropTypes.arrayOf(NotificationItemShape)
-}
 
 export default Notifications;
