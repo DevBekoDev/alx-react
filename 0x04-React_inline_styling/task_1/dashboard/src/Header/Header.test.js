b5f0490c2881fb52915ec41f8b91263d7679eea5
DevBekoDev <48620096+DevBekoDev@users.noreply.github.com>
2024-07-09 10:43:46 +0300

    done

--- 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.test.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.test.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React from "react";
+import Header from "./Header";
+import { shallow } from "enzyme";
+import { StyleSheetTestUtils } from "aphrodite";
 
-import Header from './Header';
-import { shallow } from 'enzyme';
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
 
-describe('Header component', () => {
-    it('renders without crashing', () => {
-        shallow(<Header />);
-    });
-
-    it('renders img and h1 tags', () => {
-        const wrapper = shallow(<Header />);
-        expect(wrapper.find('img')).toHaveLength(1);
-        expect(wrapper.find('h1')).toHaveLength(1);
-    });
+describe("Header", () => {
+  it("render without crashing", () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.exists()).toEqual(true);
+  });
+  it("should render a h1", () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.exists("img")).toEqual(true);
+    expect(wrapper.containsMatchingElement(<h1>School dashboard</h1>)).toEqual(true);
+  });
 });
